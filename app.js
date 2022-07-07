989cff82e3b9d3e62c076396cb9e5b06b2809b74
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-07-07 19:48:00 +0100

    google oAuth added

--- app.js
+++ app.js
@@ -1,16 +1,74 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+const session = require("express-session");
+require("dotenv").config();
+const passport = require("passport");
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.use(cors());
+/*  Google AUTH  */
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const GOOGLE_CLIENT_ID = process.env["CLIENT_ID"];
+const GOOGLE_CLIENT_SECRET = process.env["SECRET"];
 const port = process.env.PORT || 3000;
+var userProfile;
 
-app.listen(port, function() {
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
+passport.use(
+    new GoogleStrategy(
+      {
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/callback",
+      },
+      function (accessToken, refreshToken, profile, done) {
+        userProfile = profile;
+        return done(null, userProfile);
+      }
+    )
+  );
+app.use(express.static("public"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "SECRET",
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set("view engine", "ejs");
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/error" }),
+  function (req, res) {
+    // Successful authentication, redirect success.
+    res.redirect("/success");
+  }
+);
+app.get("/success", (req, res) => res.send(userProfile));
+app.get("/error", (req, res) => res.send("error logging in"));
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
 
--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 const app = require('./app.js').app;
 
-app.get('/',(req,res) => {
-    res.send("To-do home dir");
+
+app.get('/', function(req, res) {
+    res.render('pages/auth');
+  });
+  
+app.get('/policy',(req,res) => {
+    res.sendFile(__dirname + '/views/privacy.html');
 });
+
+app.get('/tos',(req,res) => {
+    res.sendFile(__dirname + '/views/tos.html');
+});
+
+//status =>  created , ok , deleted , updated , failed
+
+//returns an array of tasks [ { }, { } , ...]
+app.get('/user/:_id/tasks',(req,res) =>{
+
+})
+//query - taskname
+//responds with  { taskname , status}
+app.post('/user/:_id/tasks', (req,res) =>{
+
+})
+
+//query - task id
+//responds with { taskname , status}
+app.put('/user/:_id/tasks', (req,res) =>{
+
+})
+
+//query - task id
+//responds with { taskname , status}
+app.delete('/user/:_id/tasks', (req,res) =>{
+
+})
